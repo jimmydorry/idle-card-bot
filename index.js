0b3c91048f4399aab1dbfce77399fdfc94dea79c
jimmydorry <jimmydorry+github@gmail.com>
2014-05-13 08:35:55 +1000

    Initial push

---
+++ index.js
@@ -0,0 +1,51 @@
+var steam = require("steam"),
+    util = require("util"),
+    fs = require("fs"),
+    bot = new steam.SteamClient(),
+    appID = [];
+
+global.config = require("./config");
+
+var onSteamLogOn = function onSteamLogOn(){
+        bot.setPersonaState(steam.EPersonaState.Busy); // to display your bot's status as "Online"
+        bot.setPersonaName(config.steam_name); // to change its nickname
+        util.log("Logged on.");
+
+        //bot.gamesPlayed([appID]);
+        bot.webLogOn(function test(){});
+    },
+    onSteamSentry = function onSteamSentry(sentry) {
+        util.log("Received sentry.");
+        require('fs').writeFileSync('sentry', sentry);
+    },
+    onSteamServers = function onSteamServers(servers) {
+        util.log("Received servers.");
+        fs.writeFile('servers', JSON.stringify(servers));
+    },
+    onWebSessionID = function onWebSessionID(webSessionID) {
+        util.log("Received web session id.");
+        // steamTrade.sessionID = webSessionID;
+        bot.webLogOn(function onWebLogonSetTradeCookies(cookies) {
+            util.log("Received cookies.");
+            util.log(cookies);
+
+            /*for (var i = 0; i < cookies.length; i++) {
+                // steamTrade.setCookie(cookies[i]);
+                //util.log(i+" | "+cookies[i]);
+            }*/
+        });
+    };
+
+// Login, only passing authCode if it exists
+var logOnDetails = {
+    "accountName": config.steam_user,
+    "password": config.steam_pass,
+};
+if (config.steam_guard_code) logOnDetails.authCode = config.steam_guard_code;
+var sentry = fs.readFileSync('sentry');
+if (sentry.length) logOnDetails.shaSentryfile = sentry;
+bot.logOn(logOnDetails);
+bot.on("loggedOn", onSteamLogOn)
+    .on('sentry', onSteamSentry)
+    .on('servers', onSteamServers)
+    .on('webSessionID', onWebSessionID);
