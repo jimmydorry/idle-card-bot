80ac84b2b67bcd8c8cd3556b2403aea8944798e7
jimmydorry <jimmydorry+github@gmail.com>
2014-05-13 17:48:15 +1000

    Add in game drop availability checks.
    Added in ability to change games.

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 idle-card-bot
 =============
 
-Idle bot for trading cards
+Idle bot for trading cards. This bot will check to see what games you have card drops available for, and will run those games. Every `user definied` amount of time, the bot will re-check to see what cards are available to drop.
 
 ### Setting up
 * `npm install` in the repository root.
 * Edit `config.js` appropriately.
 * Attempt to log-in, you'll receive Error 63 - which means you need to provide a Steam Guard code.
 * Set the Steam Guard code in `config.js` and launch again.
-* After launching and successfully logging in, remove the SteamGuard code from the config
-
-## Testing
-There is no automated test suite for node-dota2 (I've no idea how I'd make one for the stuff this does :o), however there the `test` directory does contain a Steam bot with commented-out dota2 methods; you can use this bot to test the library.
-
+* After launching and successfully logging in, remove the SteamGuard code from the config.
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var steam = require("steam"),
-    util = require("util"),
-    fs = require("fs"),
-    bot = new steam.SteamClient(),
-    appID = []; //WHERE THE MAGIC HAPPENS FOR NOW
+var request = require('request')
+    , cheerio = require('cheerio')
+    , async = require('async')
+    , steam = require("steam")
+    , util = require("util")
+    , fs = require("fs")
+    , bot = new steam.SteamClient(); //WHERE THE MAGIC HAPPENS FOR NOW
 
 global.config = require("./config");
 
@@ -11,8 +13,59 @@ var onSteamLogOn = function onSteamLogOn(){
         bot.setPersonaName(config.steam_name); // to change its nickname
         util.log("Logged on.");
 
-        //bot.gamesPlayed([appID]);
-        bot.webLogOn(function test(){});
+        bot.webLogOn(function(cookies){
+            util.log("Received cookies.");
+            //util.log(cookies);
+
+            //var cookie_string = cookies.toString();
+            var cookie_string_split = cookies.toString().split(',');
+            var url = config.badge_url;
+            var j = request.jar();
+            var game_to_idle = 0;
+
+            cookie_string_split.every(function(value){
+                util.log(value);
+                j.setCookie(request.cookie(value), url);
+                return 1;
+            });
+
+            var findGameToIdle = function(callback) {
+                request({url: url, jar: j}, function (err, response, body) {
+                    game_to_idle = 0;
+                    //fs.writeFileSync('test.html', body);
+
+                    if (err) throw err;
+                    var $ = cheerio.load(body);
+                    $('span.progress_info_bold').each(function () {
+                        if (($(this).text() !== 'undefined')
+                            && ($(this).text() !== null)
+                            && ($(this).text() != 'No card drops remaining')) {
+
+                            var num_drops = $(this).text().replace(' card drops remaining', '');
+                            game_to_idle = $(this).prev().parent().children('div.badge_title_playgame').children('a.btn_green_white_innerfade').attr('href').replace('steam://run/', '');
+                            //util.log(game_to_idle + ' || ' + num_drops);
+                        }
+                    });
+                    callback(game_to_idle);
+                });
+            };
+
+            var gameToIdle_index = 0;
+            setInterval(function () {
+                util.log('Re-enumerating remaining drops');
+                findGameToIdle(function(gameToIdle){
+                    util.log('Finished poll');
+                    if(gameToIdle != gameToIdle_index){
+                        util.log('Changing idle game to: '+gameToIdle);
+                        bot.gamesPlayed([gameToIdle]);
+                        gameToIdle_index = gameToIdle;
+                    }
+                    else{
+                        util.log('Still idling: '+gameToIdle);
+                    }
+                });
+            }, config.badge_check_idle);
+        });
     },
     onSteamSentry = function onSteamSentry(sentry) {
         util.log("Received sentry.");
@@ -25,21 +78,22 @@ var onSteamLogOn = function onSteamLogOn(){
     onWebSessionID = function onWebSessionID(webSessionID) {
         util.log("Received web session id.");
         // steamTrade.sessionID = webSessionID;
-        bot.webLogOn(function onWebLogonSetTradeCookies(cookies) {
+        /*bot.webLogOn(function onWebLogonSetTradeCookies(cookies) {
             util.log("Received cookies.");
             util.log(cookies);
+            fs.writeFileSync('cookie.txt', cookies);
 
-            /*for (var i = 0; i < cookies.length; i++) {
+           //for (var i = 0; i < cookies.length; i++) {
                 // steamTrade.setCookie(cookies[i]);
                 //util.log(i+" | "+cookies[i]);
-            }*/
-        });
+            //}
+        });*/
     };
 
 // Login, only passing authCode if it exists
 var logOnDetails = {
     "accountName": config.steam_user,
-    "password": config.steam_pass,
+    "password": config.steam_pass
 };
 if (config.steam_guard_code) logOnDetails.authCode = config.steam_guard_code;
 var sentry = fs.readFileSync('sentry');
