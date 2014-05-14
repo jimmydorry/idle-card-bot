9b431c6f6a6305533318f44ae057e8f70a34a481
jimmydorry <jimmydorry@users.noreply.github.com>
2014-05-15 09:32:48 +1000

    Add idle timeout to config

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ config.steam_pass = "";
 config.steam_guard_code = "";
 
 config.badge_url = "http://steamcommunity.com/id/XXXXXXX/badges";
-config.badge_check_idle = 5 * 60 * 1000; //5mins
+config.badge_check_idle = 5 * 60 * 1000; //Check which game to idle next every 5mins
+config.badge_check_idle_no_cards = 30 * 60 * 1000; //When no cards left to idle, check every 30mins
 
 module.exports = config;
